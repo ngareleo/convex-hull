5b1643d0f760f381cd5cdf01e33c326653c2b53f
Leo Ngari <ngarimwenda@gmail.com>
2023-08-12 18:51:36 +0300

    move file reading out of impl

--- convex-hull/src/main.rs
+++ convex-hull/src/main.rs
@@ -5,7 +5,7 @@ mod fs;
 
 #[allow(dead_code)]
 fn main() {
-    let solver = Solver::new();
+    let mut solver = Solver::new();
     match solver.load_points_from_file("./res/test.txt") {
         Ok(points) => println!("{:?}", points),
         Err(msg) => println!("{msg}"),
--- convex-hull/src/solver.rs
+++ convex-hull/src/solver.rs
@@ -25,34 +25,43 @@ impl Point {
     }
 }
 
-pub struct Solver {}
+pub struct Solver {
+    pub points: Option<Vec<Point>>,
+}
 
 impl Solver {
     pub fn new() -> Self {
-        Solver {}
+        Solver {
+            points: None,
+        }
     }
 
-    pub fn load_points_from_file(self: &Solver, file_path: &str) -> Result<Vec<Point>, String> {
-        let content = fs
-            ::read_to_string(file_path)
-            .map_err(|e| format!("Error opening file. Message {e}"))?;
-        let lines = content
-            .split("\n")
-            .filter(|line| -> bool {
-                !line.starts_with("//") &&
-                    line.contains(",") &&
-                    line.split(",").collect::<Vec<&str>>().len() == 2
-            })
-            .collect::<Vec<&str>>();
-        let mut points: Vec<Point> = Vec::with_capacity(lines.len());
-        for (i, line) in lines.iter().enumerate() {
-            let point = Point::from_txt_line(line).map_err(|e| {
-                format!("Error parsing file on line {}. Message : {}", i + 1, e)
-            })?;
-            points.push(point);
-        }
-        Ok(points)
+    pub fn load_points_from_file(&mut self, file_path: &str) -> Result<(), String> {
+        self.points = Some(read_from_file(file_path)?);
+        Ok(())
     }
+}
+
+fn read_from_file(file_path: &str) -> Result<Vec<Point>, String> {
+    let content = fs
+        ::read_to_string(file_path)
+        .map_err(|e| format!("Error opening file. Message {e}"))?;
+    let lines = content
+        .split("\n")
+        .filter(|line| -> bool {
+            !line.starts_with("//") &&
+                line.contains(",") &&
+                line.split(",").collect::<Vec<&str>>().len() == 2
+        })
+        .collect::<Vec<&str>>();
+    let mut points: Vec<Point> = Vec::with_capacity(lines.len());
+    for (i, line) in lines.iter().enumerate() {
+        let point = Point::from_txt_line(line).map_err(|e| {
+            format!("Error parsing file on line {}. Message : {}", i + 1, e)
+        })?;
+        points.push(point);
+    }
+    Ok(points)
 }
 
 #[cfg(test)]
@@ -61,8 +70,7 @@ mod solver_tests {
 
     #[test]
     fn test_valid_file() {
-        let solver = Solver::new();
-        let points = solver.load_points_from_file("./res/valid_test_file.txt").unwrap();
+        let points = read_from_file("./res/valid_test_file.txt").unwrap();
         assert_eq!(points[0].as_tuple(), (2, 2));
         assert_eq!(points[1].as_tuple(), (-4, -5));
         assert_eq!(points[2].as_tuple(), (2, 0))
@@ -71,14 +79,12 @@ mod solver_tests {
     #[test]
     #[should_panic]
     fn test_invalid_file() {
-        let solver = Solver::new();
-        let _ = solver.load_points_from_file("./res/invalid_test_file.txt").unwrap();
+        let _ = read_from_file("./res/invalid_test_file.txt").unwrap();
     }
 
     #[test]
     fn test_invalid_ok_file() {
-        let solver = Solver::new();
-        let points = solver.load_points_from_file("./res/invalid_ok_test_file.txt").unwrap();
+        let points = read_from_file("./res/invalid_ok_test_file.txt").unwrap();
         assert_eq!(points[0].as_tuple(), (2, 0));
         assert_eq!(points[1].as_tuple(), (9, -1));
     }
