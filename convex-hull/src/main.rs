31036d37e107389f436e0ff150304370209da700
Leo Ngari <ngarimwenda@gmail.com>
2023-08-12 15:10:25 +0300

    file parsing added. TODO! Add fault tolerance

--- convex-hull/src/main.rs
+++ convex-hull/src/main.rs
@@ -3,7 +3,11 @@ use solver::Solver;
 mod solver;
 mod fs;
 
+#[allow(dead_code)]
 fn main() {
     let solver = Solver::new();
-    let _ = solver.load_points_from_file("./res/test.txt");
+    match solver.load_points_from_file("./res/test.txt") {
+        Ok(points) => println!("{:?}", points),
+        Err(msg) => println!("{msg}"),
+    }
 }
--- convex-hull/src/solver.rs
+++ convex-hull/src/solver.rs
@@ -3,26 +3,25 @@ use std::fs;
 // coordinates range from 0 to 100
 
 #[derive(Debug)]
-struct Point {
-    x: usize,
-    y: usize,
+pub struct Point {
+    x: i32,
+    y: i32,
 }
 
 impl Point {
-    pub fn new(x: usize, y: usize) -> Self {
+    pub fn new(x: i32, y: i32) -> Self {
         Point { x, y }
     }
 
-    pub fn from_txt_line(line: &str) -> Option<Self> {
-        if line.starts_with("//") {
-            return None;
-        }
+    pub fn as_tuple(self: &Self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    pub fn from_txt_line(line: &str) -> Result<Self, String> {
         let xy: Vec<&str> = line.trim().split(",").collect();
-        println!("{:?}", xy);
-        match xy.len() {
-            2 => Some(Point::new(xy[0].parse().unwrap(), xy[1].parse().unwrap())),
-            _ => None,
-        }
+        let x = xy[0].parse().map_err(|_| format!("Invalid X coordinate given. xy is {:?}", xy))?;
+        let y = xy[1].parse().map_err(|_| format!("Invalid Y coordinate given. xy is {:?}", xy))?;
+        Ok(Point::new(x, y))
     }
 }
 
@@ -33,16 +32,25 @@ impl Solver {
         Solver {}
     }
 
-    pub fn load_points_from_file(self: &Solver, file_path: &str) {
-        let content = fs::read_to_string(file_path).expect("Error reading file");
-        let points: &Vec<Option<Point>> = &content
+    pub fn load_points_from_file(self: &Solver, file_path: &str) -> Result<Vec<Point>, String> {
+        let content = fs
+            ::read_to_string(file_path)
+            .map_err(|e| format!("Error opening file. Message {e}"))?;
+        let lines = content
             .split("\n")
-            .map(|val| Point::from_txt_line(val))
-            .filter(|val| val.is_some())
-            .collect();
-
-        for (i, point) in points.iter().enumerate() {
-            println!("{i}: {:?}", point);
+            .filter(|line| -> bool {
+                !line.starts_with("//") &&
+                    line.contains(",") &&
+                    line.split(",").collect::<Vec<&str>>().len() == 2
+            })
+            .collect::<Vec<&str>>();
+        let mut points: Vec<Point> = Vec::with_capacity(lines.len());
+        for (i, line) in lines.iter().enumerate() {
+            let point = Point::from_txt_line(line).map_err(|e| {
+                format!("Error parsing file on line {}. Message : {}", i + 1, e)
+            })?;
+            points.push(point);
         }
+        Ok(points)
     }
 }
